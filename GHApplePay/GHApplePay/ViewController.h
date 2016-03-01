a1464554bb7b9cf2ec98b2747c73b94b870b6a79
joinup <shen_sgh@icloud.com>
2016-03-01 12:57:36 +0800

    create GHApplePay

---
+++ GHApplePay/GHApplePay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHApplePay
+//
+//  Created by shen_gh on 16/3/1.
+//  Copyright © 2016年 com.joinup(Beijing). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
